d3b070aee38d2c4ac80dda29721f75df7e45320b
geppetto <saejinpark951210@gmail.com>
2023-11-26 22:22:20 +0900

    Title: 특정 조건 세 정수 비교, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 231126/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let (a, b, c) = {
+    let nums = readLine()!.split(separator: " ").map{Int($0)!}
+
+    return (nums[0], nums[1], nums[2])
+}()
+
+print(a < b && a < c ? 1 : 0, a == b && b == c ? 1 : 0)
