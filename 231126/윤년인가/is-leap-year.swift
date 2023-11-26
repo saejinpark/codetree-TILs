1a1da78ad1d090d265c0665e41503d06c0328207
geppetto <saejinpark951210@gmail.com>
2023-11-26 23:48:40 +0900

    Title: 윤년인가, Time: 13ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 231126/윤년인가/is-leap-year.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+let y = Int(readLine()!)!
+
+func isLeapYear(year: Int) -> Bool {
+    if year % 4 != 0 {
+        return false
+    }
+
+    if year >= 400 && year % 400 == 0 {
+        return true
+    }
+
+    if year >= 100 && year % 100 == 0 {
+        return false
+    }
+
+    return true
+}
+
+print(isLeapYear(year: y))
