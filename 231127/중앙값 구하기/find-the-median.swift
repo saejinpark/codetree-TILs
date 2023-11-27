3a657761d939ee7c972357e70f7575722a12ce9e
geppetto <saejinpark951210@gmail.com>
2023-11-27 19:05:56 +0900

    Title: 중앙값 구하기, Time: 12ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 231127/중앙값 구하기/find-the-median.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+var (a, b, c) = {
+    let nums = readLine()!.split(separator: " ").map{Int($0)!}
+    return (nums[0], nums[1], nums[2])
+}()
+
+if a > b {
+    (a, b) = (b, a)
+}
+
+if b > c {
+    (b, c) = (c, b)
+}
+
+print(b)
