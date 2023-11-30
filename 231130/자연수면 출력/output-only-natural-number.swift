805cfcdc62da353095c8ecdc0d6f9b5f58eb0dd3
geppetto <saejinpark951210@gmail.com>
2023-11-30 14:25:16 +0900

    Title: 자연수면 출력, Time: 13ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 231130/자연수면 출력/output-only-natural-number.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+precedencegroup FowardPipe {
+    associativity: left
+}
+
+infix operator |> : FowardPipe
+
+public func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+let (a, b) = readLine()!.split(separator: " ").map{Int($0)!} |> {($0[0], $0[1])}
+
+func solution(_ a: Int, _ b: Int) -> String {
+    return a < 0 ? "0" : (1...b).map{_ in return String(a)}.joined(separator: "")
+}
+
+let answer = solution(a, b)
+
+print(answer)
