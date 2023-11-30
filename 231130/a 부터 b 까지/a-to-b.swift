349b1eb535a1b7bb520209e02322bbc14da032ee
geppetto <saejinpark951210@gmail.com>
2023-11-30 15:02:34 +0900

    Title: a 부터 b 까지, Time: 12ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 231130/a 부터 b 까지/a-to-b.swift
+++ 231130/a 부터 b 까지/a-to-b.swift
@@ -13,13 +13,12 @@ public func |> <T, U> (value: T, function : (T) -> U) -> U {
 let (a, b) = readLine()!.split(separator: " ").map{Int($0)!} |> {($0[0], $0[1])}
 
 func solution(_ a: Int, _ b: Int) -> String {
-    var arr = [Int](), current = a
+    var arr = [Int]([a]), current = a
     
     while current < b{
-        arr.append(current)
         current = current % 2 == 0 ? current + 3 : current * 2
+        arr.append(current)
     }
-    arr.append(current)
 
     return arr.map{String($0)}.joined(separator: " ") 
 }
