21615e6c56a286a1d311cdaa531471600097adc2
geppetto <saejinpark951210@gmail.com>
2023-11-30 12:55:09 +0900

    Title: 짝수만 출력 2, Time: 10ms, Memory: 1MB, Status: Passed - CodeTree

--- 231130/짝수만 출력 2/output-only-even-2.swift
+++ 231130/짝수만 출력 2/output-only-even-2.swift
@@ -10,4 +10,20 @@ public func |> <T, U> (value: T, function: ((T) -> U)) -> U {
     return function(value)
 }
 
-let n = readLine()! |> {$0.split(separator: " ")} |>
+let (a, b) = readLine()!.split(separator: " ").map{Int($0)!} |> {($0[1], $0[0])}
+
+func solution(_ a: Int, _ b: Int) -> String {
+    let start = b % 2 == 1 ? b - 1 : b
+    let end = a - 1
+    var arr = [Int]()
+    var i = start
+    
+    while i > end {
+        arr.append(i)
+        i -= 2
+    }
+
+    return arr |> {$0.map{String($0)}} |> {$0.joined(separator: " ")}
+}
+
+print(solution(a, b))
