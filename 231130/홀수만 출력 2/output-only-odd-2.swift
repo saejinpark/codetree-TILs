6af6f9a9c3bb6324cafe3d2de484f78c60bc3201
geppetto <saejinpark951210@gmail.com>
2023-11-30 11:37:44 +0900

    Title: 홀수만 출력 2, Time: 13ms, Memory: 1MB, Status: Passed - CodeTree

--- 231130/홀수만 출력 2/output-only-odd-2.swift
+++ 231130/홀수만 출력 2/output-only-odd-2.swift
@@ -1,13 +1,27 @@
 import Foundation
 
-let (b, a) = {
-    let nums = readLine()!.split(separator: " ").map{Int($0)!}
-    return (nums[0], nums[1])
-}()
-
-func solution(_ a: Int, _ b: Int) {
-    var (start, end) = (b % 2 == 0 ? b - 1 : b, a - 1)
-    print(stride(from: start, to: end, by: -2).map{String($0)}.joined(separator: " "))
+precedencegroup ForwardPipe {
+    associativity: left
 }
 
-solution(a, b)
+infix operator |> : ForwardPipe
+
+public func |> <T, U>(value: T, function: ((T) -> U)) -> U {
+    return function(value)
+}
+
+let (a, b) = readLine()! |> {$0.split(separator: " ")} |> {$0.map{Int($0)!}} |> {($0[1], $0[0])}
+
+func solution(_ a : Int, _ b : Int) -> String {
+    let start = b % 2 == 1 ? b : b - 1
+    let end = a - 1
+    var i = start
+    var arr = [Int]()
+    while i > end {
+        arr.append(i)
+        i -= 2
+    }
+    return arr |> {$0.map{String($0)}} |> {$0.joined(separator: " ")}
+}
+
+print((a, b) |> {solution($0, $1)})
