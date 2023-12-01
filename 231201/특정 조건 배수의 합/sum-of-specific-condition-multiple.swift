f0e8da903d9e4e9fef802c8b95773292143e3e7c
geppetto <saejinpark951210@gmail.com>
2023-12-01 14:23:25 +0900

    Title: 특정 조건 배수의 합, Time: 21ms, Memory: 1MB, Status: Runtime Error - CodeTree

--- 231201/특정 조건 배수의 합/sum-of-specific-condition-multiple.swift
+++ 231201/특정 조건 배수의 합/sum-of-specific-condition-multiple.swift
@@ -10,4 +10,14 @@ public func |> <T, U> (value: T, function: (T) -> U) -> U {
     return function(value)
 }
 
-let (a, b) = readLine()!.split(separator: "").map{Int($0)!} |> {($0[0], $0[1])}
+let (a, b) = readLine()!.split(separator: " ").map{Int($0)!} |> {($0[0], $0[1])}
+let sum = {(nums: [Int]) -> Int in nums.reduce(0){$0 + $1}}
+
+
+func solution(_ a : Int, _ b : Int ) -> Int {
+    return (a...b).filter{$0 % 5 == 0} |> sum
+}
+
+let answer = solution(a, b)
+
+print(answer)
