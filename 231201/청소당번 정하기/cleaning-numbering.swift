7310ac76003c0ffdb9dcddbe56b5974bd8b7a4c4
geppetto <saejinpark951210@gmail.com>
2023-12-01 13:01:28 +0900

    Title: 청소당번 정하기, Time: 10ms, Memory: 1MB, Status: Passed - CodeTree

--- 231201/청소당번 정하기/cleaning-numbering.swift
+++ 231201/청소당번 정하기/cleaning-numbering.swift
@@ -2,24 +2,30 @@ import Foundation
 
 let n = Int(readLine()!)!
 
-var days = (0...n).map{_ in return -1}
+func solution(_ n: Int) -> String {
+    var days = (0...n).map{_ in return -1}
 
-let periods = [2, 3, 12]
+    let periods = [2, 3, 12]
 
-for i in 0..<periods.count {
-    let period = periods[i]
-    for day in stride(from: period, to: n + 1, by: period) {
-        days[day] = i
+    for i in 0..<periods.count {
+        let period = periods[i]
+        for day in stride(from: period, to: n + 1, by: period) {
+            days[day] = i
+        }
     }
+
+    var counts = [0, 0, 0]
+
+    for day in days {
+        if day == -1 {
+            continue
+        }
+        counts[day] += 1
+    }
+
+    return counts.map{String($0)}.joined(separator: " ")
 }
 
-var counts = [0, 0, 0]
+let answer = solution(n)
 
-for day in days {
-    if day == -1 {
-        continue
-    }
-    counts[day] += 1
-}
-
-print(counts.map{String($0)}.joined(separator: " "))
+print(answer)
