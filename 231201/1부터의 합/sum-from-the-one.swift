1a606fc47dccbc3e55204fdebbb0ddce244aa9a1
geppetto <saejinpark951210@gmail.com>
2023-12-01 16:42:35 +0900

    Title: 1부터의 합, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 231201/1부터의 합/sum-from-the-one.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+func solution(_ n: Int) -> Int {
+    var sum = 0
+    for i in 1...100 {
+        sum += i
+        if sum > n {
+            return i
+        }
+    }
+    return 0
+}
+
+let answer = solution(n)
+
+print(answer)
