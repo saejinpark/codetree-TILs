3054975c7478135d7219dd7ee71aedf599a4c7d4
geppetto <saejinpark951210@gmail.com>
2023-12-01 12:58:17 +0900

    Title: 청소당번 정하기, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 231201/청소당번 정하기/cleaning-numbering.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+var days = (0...n).map{_ in return -1}
+
+let periods = [2, 3, 12]
+
+for i in 0..<periods.count {
+    let period = periods[i]
+    for day in stride(from: period, to: n + 1, by: period) {
+        days[day] = i
+    }
+}
+
+var counts = [0, 0, 0]
+
+for day in days {
+    if day == -1 {
+        continue
+    }
+    counts[day] += 1
+}
+
+print(counts.map{String($0)}.joined(separator: " "))
