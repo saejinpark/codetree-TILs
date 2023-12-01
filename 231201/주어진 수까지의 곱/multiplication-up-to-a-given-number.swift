90b682eac7f2494320019774cfe8095ca77ed752
geppetto <saejinpark951210@gmail.com>
2023-12-01 15:41:46 +0900

    Title: 주어진 수까지의 곱, Time: 15ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 231201/주어진 수까지의 곱/multiplication-up-to-a-given-number.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+let (a, b) = readLine()!.split(separator: " ").map{Int($0)!} |> {($0[0], $0[1])}
+
+func solution(_ a: Int, _ b: Int) -> Int {
+    return (a...b).reduce(1, *)
+}
+
+let answer = solution(a, b)
+
+print(answer)
