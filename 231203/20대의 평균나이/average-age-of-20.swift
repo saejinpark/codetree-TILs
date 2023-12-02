8669f73d0eaace23b77a5d656917754c857cbaf9
geppetto <saejinpark951210@gmail.com>
2023-12-03 00:16:24 +0900

    Title: 20대의 평균나이, Time: 21ms, Memory: 1MB, Status: Runtime Error - CodeTree

---
+++ 231203/20대의 평균나이/average-age-of-20.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+let sum = {(nums: [Int]) -> Int in nums.reduce(0, +)}
+let avg = {(nums: [Int]) -> Float in round(Float(sum(nums)) / Float(nums.count) * 100) / 100 }
+
+func solution() -> String {
+    var ages = [Int]()
+    while true {
+        let age = Int(readLine()!.trimmingCharacters(in: [" "]))!
+
+        switch age {
+            case 1...29:
+                ages.append(age)
+                break
+            default:
+                return String(format: "%.2f", avg(ages))
+        }
+    }
+}
+
+
+let answer = solution()
+
+print(answer)
