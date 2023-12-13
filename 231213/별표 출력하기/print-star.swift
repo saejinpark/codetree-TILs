4234cc46d4c0fce30e907db54148da76914216c1
geppetto <saejinpark951210@gmail.com>
2023-12-13 14:36:24 +0900

    Title: 별표 출력하기, Time: 14ms, Memory: 1MB, Status: Passed - CodeTree

--- 231213/별표 출력하기/print-star.swift
+++ 231213/별표 출력하기/print-star.swift
@@ -4,6 +4,9 @@ let n = Int(readLine()!)!
 
 
 func solution(_ n : Int) -> String {
+    if n == 1 {
+        return "*"
+    }
     return ((1...n) + (1...n - 1).map{n - $0}).map{Array(repeating: "*", count: $0).joined(separator: " ")}.joined(separator: "\n")
 }
 
