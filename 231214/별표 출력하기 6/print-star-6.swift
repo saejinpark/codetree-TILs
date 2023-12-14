136ae4cc3e8cab680a22026dc1e1fb538ccdbbe6
geppetto <saejinpark951210@gmail.com>
2023-12-14 13:41:16 +0900

    Title: 별표 출력하기 6, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

--- 231214/별표 출력하기 6/print-star-6.swift
+++ 231214/별표 출력하기 6/print-star-6.swift
@@ -11,6 +11,9 @@ func generateSpace(_ n : Int) -> String {
 }
 
 func solution(_ n : Int) -> String {
+    if n == 1 {
+        return "*"
+    }
     return ((1...n).reversed() + (2...n)).map{
         let starLen = $0 * 2 - 1
         let spaceLen = n * 2 - 1 - starLen
