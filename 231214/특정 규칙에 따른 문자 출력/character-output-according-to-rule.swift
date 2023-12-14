13dae930fdce3da6390007b1ef0018380cc851de
geppetto <saejinpark951210@gmail.com>
2023-12-14 13:58:40 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 231214/특정 규칙에 따른 문자 출력/character-output-according-to-rule.swift
+++ 231214/특정 규칙에 따른 문자 출력/character-output-according-to-rule.swift
@@ -3,6 +3,9 @@ import Foundation
 let n = Int(readLine()!)!
 
 func solution(_ n : Int) -> String {
+    if n == 1 {
+        return "*"
+    }
     return ((1...n).map{
         (Array(repeating: " ", count: n - $0) + Array(repeating: "@", count: $0)).joined(separator: " ")
     } + (1...n - 1).reversed().map{
