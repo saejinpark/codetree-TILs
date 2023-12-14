262f64c1530f1a7f701fdf35c6421a9464aca6fc
geppetto <saejinpark951210@gmail.com>
2023-12-14 13:40:42 +0900

    Title: 별표 출력하기 6, Time: 21ms, Memory: 1MB, Status: Runtime Error - CodeTree

---
+++ 231214/별표 출력하기 6/print-star-6.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+func generateStarPattern(_ n : Int) -> String {
+    return Array(repeating: "*", count: n).joined(separator: " ")
+}
+
+func generateSpace(_ n : Int) -> String {
+    return String(repeating: " ", count: n)
+}
+
+func solution(_ n : Int) -> String {
+    return ((1...n).reversed() + (2...n)).map{
+        let starLen = $0 * 2 - 1
+        let spaceLen = n * 2 - 1 - starLen
+        return generateSpace(spaceLen) + generateStarPattern(starLen) + generateSpace(spaceLen)
+    }.joined(separator: "\n")
+}
+
+let answer = solution(n)
+
+print(answer)
