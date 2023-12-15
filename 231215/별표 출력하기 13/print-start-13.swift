d9f4b8670a5b972644585dc66c2c180ab21b3b2f
geppetto <saejinpark951210@gmail.com>
2023-12-15 22:43:52 +0900

    Title: 별표 출력하기 13, Time: 12ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 231215/별표 출력하기 13/print-start-13.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+func solution(_ n : Int) -> String {
+    return (1...3).map{
+        [
+            Array(repeating: "*", count: n - $0 + 1).joined(separator: " "),
+            Array(repeating: "*", count: $0).joined(separator: " ")
+        ].joined(separator: "\n")
+    }.joined(separator: "\n")
+} 
+
+let answer = solution(n)
+
+print(answer)
