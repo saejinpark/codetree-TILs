bad577487304b0f87d7e14b8d553ca5c9bbc9234
geppetto <saejinpark951210@gmail.com>
2024-04-14 15:04:20 +0900

    Title: 구구단으로 이루어진 직각 삼각형, Time: 12ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240414/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+func input() -> Int? {
+    guard let line = readLine(), let result = Int(line)
+    else {
+        return nil
+    }
+    return result
+}
+
+func solution(_ n: Int) -> String {
+    return (1...n).map{ i in
+        (1...(n - i + 1)).map{ j in
+            "\(i) * \(j) = \(i * j)"
+        }.joined(separator: " / " )
+    }.joined(separator: "\n")
+}
+
+func main() {
+    if let n = input() {
+        print(solution(n))
+    }
+}
+
+main()
