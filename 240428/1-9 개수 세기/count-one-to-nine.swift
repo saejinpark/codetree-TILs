0db4b9b3019ebd0c9cd8d6e0a95c3254fa1dcfda
geppetto <saejinpark951210@gmail.com>
2024-04-28 20:53:40 +0900

    Title: 1-9 개수 세기, Time: 13ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240428/1-9 개수 세기/count-one-to-nine.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value:T, function:(T) -> U) -> U {
+    return function(value)
+}
+
+func readNum() -> Int? {
+    guard let line = readLine(), let num = Int(line) else { return nil }
+    return num
+}
+
+func readNums() -> [Int]? {
+    guard let line = readLine() else { return nil }
+    let temp = line.split(separator: " ")
+    let nums = temp.compactMap({ Int($0) })
+    return temp.count == nums.count ? nums : nil
+}
+
+func formatAnswer(nums:[Int]) -> String {
+    return nums
+            .map{ String($0) }
+            .joined(separator: "\n")
+}
+
+func solution(_ n:Int, _ nums:[Int]) -> [Int] {
+    var arr = Array(repeating: 0, count: 9)
+    for num in nums {
+        arr[num - 1] += 1
+    }
+    return arr
+}
+
+func main() {
+    if let n = readNum(), let nums = readNums() {
+        let answer = solution(n, nums)
+        print(answer |> formatAnswer)
+    }
+}
+
+main()
