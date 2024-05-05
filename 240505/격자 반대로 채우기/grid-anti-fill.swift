5f2ef3afdd75909ee0c36ae45426b7f6168b5961
geppetto <saejinpark951210@gmail.com>
2024-05-05 15:30:47 +0900

    Title: 격자 반대로 채우기, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240505/격자 반대로 채우기/grid-anti-fill.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value:T, function:(T) -> U) -> U {
+    return function(value)
+}
+
+func readNum() -> Int? {
+    guard let line = readLine(), let num = Int(line)
+    else { return nil }
+    return num
+}
+
+func genNumFunc() -> () -> Int {
+    var num = 1
+    return {
+        defer {
+            num += 1
+        }
+        return num
+    }    
+} 
+
+func solution(n:Int) -> [[Int]] {
+    var answer = Array(
+                repeating: Array(repeating: 0, count: n),
+                count: n
+                )
+    let genNum = genNumFunc()
+    for c in stride(from: n - 1, to: -1, by: -1) {
+        if c % 2 == 0 {
+            for r in 0..<n {
+                answer[r][c] = genNum()
+            }
+            continue
+        }
+        for r in stride(from: n - 1, to: -1, by: -1) {
+            answer[r][c] = genNum()
+        }
+    }
+    return answer
+}
+
+func answerFormat(answer:[[Int]]) -> String {
+    return answer.map{$0.map{String($0)}.joined(separator:" ")}.joined(separator:"\n")
+}
+
+func main() {
+    if let n = readNum() {
+        print(n |> solution |> answerFormat)
+    }
+}
+
+main()
