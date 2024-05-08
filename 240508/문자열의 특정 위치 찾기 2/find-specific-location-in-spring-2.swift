710a3dd50d83e687c9b13c155921ac0c00effbdc
geppetto <saejinpark951210@gmail.com>
2024-05-08 21:46:32 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 12ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240508/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value:T, function:(T) -> U) -> U {
+    return function(value)
+}
+
+let fruits = ["apple", "banana", "grape", "blueberry", "orange"]
+
+func solution(str:String) -> String {
+    let selectedFruits = fruits.map{$0.split(separator:"")}.filter{$0[2] == str || $0[3] == str}.map{$0.joined(separator:"")}
+    return "\(selectedFruits.joined(separator:"\n"))\n\(selectedFruits.count)"
+}
+
+func main() {
+    if let str = readLine() {
+        print(str |> solution)
+    }
+}
+
+main()
