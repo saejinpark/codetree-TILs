36270761a9f26fc4da8fbfa5b36830eddc2743f1
geppetto <saejinpark951210@gmail.com>
2024-05-09 19:39:57 +0900

    Title: 문자열에서 문자 출력, Time: 16ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240509/문자열에서 문자 출력/print-chars-from-word.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value:T, function:(T) -> U) -> U {
+    return function(value)
+}
+
+func solution(line:String) -> String {
+    return line.split(separator: "").joined(separator: "\n")
+}
+
+func main() {
+    if let line = readLine() {
+        print(line |> solution)
+    }
+}
+
+main()
