63903782edf27448ed000570cf093dc71b81cb2a
geppetto <saejinpark951210@gmail.com>
2024-05-20 17:42:49 +0900

    Title: 첫 번째와 두 번째 교환, Time: 13ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240520/첫 번째와 두 번째 교환/exchange-1st-and-2nd.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value:T, function:(T) -> U) -> U {
+    return function(value)
+}
+
+func solution(str:String) -> String {
+    let strArr1 = Array(str).map{String($0)}
+    var strArr2 = strArr1[..<strArr1.endIndex]
+    for i in 0..<strArr2.count {
+        if strArr2[i] == strArr1[0] {
+            strArr2[i] = strArr1[1]
+        }else if strArr2[i] == strArr1[1] {
+            strArr2[i] = strArr1[0]
+        }
+    }
+    return strArr2.joined(separator: "")
+}
+
+func main() {
+    if let line = readLine() {
+        print(line |> solution)
+    }
+}
+
+main()
