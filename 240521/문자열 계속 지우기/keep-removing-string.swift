17ea51b75c3d1dc2b5fd27c372e6949782395bb9
geppetto <saejinpark951210@gmail.com>
2024-05-21 14:58:36 +0900

    Title: 문자열 계속 지우기, Time: 1082ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

---
+++ 240521/문자열 계속 지우기/keep-removing-string.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value:T, function:(T) -> U) -> U {
+    return function(value)
+}
+
+func solution(component:(String, String)) -> String {
+    let (str, base) = component
+    var strArr = str.split(separator: base).map{String($0)}
+    while strArr.count != 1 {
+        strArr = strArr.joined(separator: "").split(separator: base).map{String($0)}
+    }
+    return strArr[0]
+}
+
+func main() {
+    if let line1 = readLine(), let line2 = readLine() {
+        print((line1, line2) |> solution)
+    }
+}
+
+main()
