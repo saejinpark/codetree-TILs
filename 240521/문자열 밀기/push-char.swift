1b91aa3af6cddef13ca5359680a141f07c0edc5d
geppetto <saejinpark951210@gmail.com>
2024-05-21 15:18:13 +0900

    Title: 문자열 밀기, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240521/문자열 밀기/push-char.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity : left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value:T, function:(T) -> U) -> U {
+    return function(value)
+}
+
+func solution(str:String) -> String {
+    var strArr = Array(str).map(String.init)
+    strArr.append(strArr.remove(at: 0))
+    return strArr.joined(separator: "")
+}
+
+func main() {
+    if let line = readLine() {
+        print(line |> solution)
+    }
+}
+
+main()
