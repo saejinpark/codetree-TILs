9c3b560c5c0d8f26d3b16d2733baffcc85d5f172
geppetto <saejinpark951210@gmail.com>
2024-05-22 12:22:34 +0900

    Title: 아스키코드의 합과 차, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240522/아스키코드의 합과 차/sum-and-subtract-in-ASCII.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value:T, function:(T) -> U) -> U {
+    return function(value)
+}
+
+func readTwoStr() -> (String, String)? {
+    guard let line = readLine() else {return nil}
+    let strs = line.split(separator: "")
+    if strs.count != 2 {
+        return nil
+    }
+    return strs.map(String.init) |> {($0[0], $0[1])}
+}
+
+func singleStrToAscii(_ str:String) -> Int? {
+    if str.count != 1 { return nil }
+    let char = Character(str)
+    guard let ascii = char.asciiValue else { return nil }
+    return Int(ascii)
+}
+
+func solution(twoStr:(String, String)) -> String {
+    let (str1, str2) = twoStr
+    if let ascii1 = singleStrToAscii(str1), let ascii2 = singleStrToAscii(str2) {
+        return "\(ascii1) \(ascii2)"
+    }
+    return "Error"
+}
+
+func main() {
+    if let input = readTwoStr() {
+        print(input |> solution)
+    }
+}
+
+main()
