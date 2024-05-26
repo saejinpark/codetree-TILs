48534ca9df4c4bb9cdfd4e884636c2846bfa6555
geppetto <saejinpark951210@gmail.com>
2024-05-26 14:03:29 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240526/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+func readNum() -> Int? {
+    guard let line = readLine(), let num = Int(line) else { return nil } 
+    return num
+}
+
+func solution(n: Int) -> String {
+    if n % 2 != 0 { return "1 No" }
+    let posNumSum = String(n).split(separator: "").compactMap({Int($0)}).reduce(0, +)
+    if posNumSum % 5 != 0 { return " 2 No" }
+    return "Yes"
+}
+
+func main() {
+    guard let n = readNum() else { return }
+    print(n |> solution)
+}
+
+main()
