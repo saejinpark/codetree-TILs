87c476c93c03838a745b69ac666ce3a7af9d5963
geppetto <saejinpark951210@gmail.com>
2024-06-01 00:19:09 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 18ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240601/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+func readNum() -> Int? {
+    guard let line = readLine(), let num = Int(line) else { return nil}
+    return num
+}
+
+func solution(num: Int) -> Int {
+    if num < 3 { return 1 }
+    return solution(num: num - 2) + solution(num: num - 1)
+}
+
+func main() {
+    guard let num = readNum() else { return }
+    print(num |> solution)
+}
+
+main()
