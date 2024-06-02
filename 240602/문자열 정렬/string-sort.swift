7d84a17618566728431faffd3eaca9e9d4239b7b
geppetto <saejinpark951210@gmail.com>
2024-06-02 19:11:44 +0900

    Title: 문자열 정렬, Time: 12ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240602/문자열 정렬/string-sort.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+func solution(str: String) -> String {
+    return str.split(separator: "").map(String.init).sorted().joined(separator: "")
+}
+
+func main() {
+    guard let line = readLine() else { return }
+    print(line |> solution)
+}
+
+main()
