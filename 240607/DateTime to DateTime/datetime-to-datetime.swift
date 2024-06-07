2bba9d5f29bbdfa19094e6b5a80bb3121c46fe52
geppetto <saejinpark951210@gmail.com>
2024-06-07 15:04:42 +0900

    Title: DateTime to DateTime, Time: 13ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240607/DateTime to DateTime/datetime-to-datetime.swift
+++ 240607/DateTime to DateTime/datetime-to-datetime.swift
@@ -18,13 +18,15 @@ func readDayMonthDay() -> (Int, Int, Int)? {
     return (nums[0], nums[1], nums[2])
 }
 
-func getMinute(day: Int, hour: Int, minute: Int) -> Int {
+func getMinute(input: (Int, Int, Int)) -> Int {
+    let (day, hour, minute) = input
     return (day * 24 * 60) + (hour * 60) + minute
 }
 
 func solution(input: (Int, Int, Int)) -> Int {
-    let (day, hour, minute) = input
-    return getMinute(day: day, hour: hour, minute: minute) - getMinute(day: 11, hour: 11, minute: 11)
+    let start = (11, 11, 11) |> getMinute
+    let end = input |> getMinute
+    return start < end ? end - start : -1
 }
 
 func main() {
