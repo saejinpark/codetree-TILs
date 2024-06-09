8c44b2e966b59d9bc040207383e869d09169de43
geppetto <saejinpark951210@gmail.com>
2024-06-09 12:36:56 +0900

    Title: 왔다 갔던 구역 2, Time: 12ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240609/왔다 갔던 구역 2/area-been-to-and-from2.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+func readNum() -> Int? {
+    guard let line = readLine(), let num = Int(line) else { return nil }
+    return num
+}
+
+func readArrow() -> (Int, String)? {
+    guard let line = readLine() else { return nil }
+    let temp = line.split(separator: " ")
+    if temp.count == 2, let num = Int(temp[0]) {
+        return (num, String(temp[1]))
+    }
+    return nil
+}
+
+func solution(input: (Int, [(Int, String)])) -> Int {
+    let (n, arrows) = input
+    let constant = 100
+    var arr = Array(repeating: 0, count: 1200)
+    var current = constant
+    for (num, dict) in arrows {
+        switch dict {
+        case "L":
+            for i in 0..<num {
+                current = current - 1
+                arr[current] += 1
+            }
+        case "R":
+            for i in 0..<num {
+                current = current + 1
+                arr[current] += 1
+            }
+        default:
+            return -1
+        }
+    }
+
+    return arr.filter{$0 >= 2}.count
+}
+
+func main() {
+    guard let n = readNum() else { return }
+    let arrows = (1...n).compactMap({_ in readArrow()})
+    print((n, arrows) |> solution)
+}
+
+main()
