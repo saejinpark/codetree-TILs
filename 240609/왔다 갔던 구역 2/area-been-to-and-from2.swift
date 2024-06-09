ed6a5ea9fc11e8117b952353b03bc668581a4c1e
geppetto <saejinpark951210@gmail.com>
2024-06-09 13:39:43 +0900

    Title: 왔다 갔던 구역 2, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240609/왔다 갔던 구역 2/area-been-to-and-from2.swift
+++ 240609/왔다 갔던 구역 2/area-been-to-and-from2.swift
@@ -29,7 +29,6 @@ func solution(input: (Int, [(Int, String)])) -> Int {
     let constant = 300
     var arr = Array(repeating: 0, count: 600)
     var current = constant
-    arr[current] = 1
     for (num, dict) in arrows {
         switch dict {
         case "L":
