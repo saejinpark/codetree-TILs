fdbe57395ba606de4cffc9e7dfd4cc26ff814bce
geppetto <saejinpark951210@gmail.com>
2024-06-12 21:02:53 +0900

    Title: 연속되는 수 4, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240612/연속되는 수 4/continuous-number4.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+func readNum() -> Int? {
+    guard let line = readLine(), let num = Int(line) else { return nil }
+    return num
+}
+
+func solution(nums: [Int]) -> Int {
+    var maxLength = 0
+    var length = 0
+    var temp = -1
+    for num in nums {
+        if temp == -1 || num <= temp {
+            temp = num
+            length = 1
+            if maxLength == 0 {
+                maxLength == 1
+            }
+            continue
+        }
+        temp = num
+        length += 1
+        maxLength = length > maxLength ? length : maxLength
+    }
+    return maxLength
+}
+
+func main() {
+    guard let n = readNum() else { return }
+    let nums = (1...n).compactMap({_ in readNum()})
+    if n != nums.count { return }
+    print(nums |> solution) 
+}
+
+main()
