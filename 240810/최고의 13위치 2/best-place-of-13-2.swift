4514d61e0e0cad074da6b73bb888d054178404f9
geppetto <saejinpark951210@gmail.com>
2024-08-10 21:50:42 +0900

    Title: 최고의 13위치 2, Time: 10ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240810/최고의 13위치 2/best-place-of-13-2.swift
@@ -0,0 +1,113 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+func readNum() -> Int? {
+    guard let line = readLine(), let num = Int(line) else { return nil }
+    return num
+}
+
+func readNums() -> [Int]? {
+    guard let line = readLine() else { return nil }
+    let temp = line.split(separator: " ")
+    let nums = temp.compactMap({Int($0)})
+    return temp.count == nums.count ? nums : nil
+}
+
+func readGrid(n: Int) -> [[Int]]? {
+    var grid = [[Int]]()
+    for i in 1...n {
+        guard let nums = readNums(), nums.count == n else { return nil }
+        grid.append(nums)
+    }
+
+    return grid
+}
+
+let genId = {
+    var id = 0
+    return {
+        defer {
+            id += 1
+        }
+        return id
+    }
+}()
+
+class Coord: Hashable {
+    let id: Int
+    let row: Int
+    let col: Int
+
+    init(row: Int, col: Int) {
+        self.id = genId()
+        self.row = row
+        self.col = col
+    }
+
+    static func == (lhs: Coord, rhs: Coord) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(row)
+        hasher.combine(col)
+    }
+
+    func find(grid: [[Int]]) -> Int {
+        return grid[row][col]
+    }
+}
+
+typealias CoordSet = Set<Coord>
+
+func solution(component: (Int, [[Int]])) -> Int {
+    let (n, grid) = component
+    var setArr = [CoordSet]()
+    var answer = 0
+
+    for i in 0..<n {
+        for j in 2..<n {
+            var coordSet = CoordSet()
+            
+            coordSet.insert(Coord(row: i, col: j - 2))
+            coordSet.insert(Coord(row: i, col: j - 1))
+            coordSet.insert(Coord(row: i, col: j))
+
+            setArr.append(coordSet)
+        }
+    }
+    for i in 0..<setArr.count {
+        for j in (i + 1)..<setArr.count {
+            let first = setArr[i]
+            let second = setArr[j]
+            let unionSet = first.union(second)
+            if unionSet.count == 6 {
+                var testCase = 0
+                for item in unionSet {
+                    testCase += item.find(grid: grid)
+                }
+                answer = testCase > answer ? testCase : answer
+
+            }
+        }
+    }
+
+    return answer
+}
+
+func main() {
+    guard let n = readNum(), let grid = readGrid(n: n) else { return }
+    print((n, grid) |> solution)
+}
+
+main()
