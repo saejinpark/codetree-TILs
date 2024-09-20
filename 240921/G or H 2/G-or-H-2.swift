96ac4a2f878a3efe4de1c02783b788e2db2189f5
geppetto <saejinpark951210@gmail.com>
2024-09-21 00:45:03 +0900

    Title: G or H 2, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240921/G or H 2/G-or-H-2.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+func readNum() -> Int? {
+    guard let line = readLine(), let num = Int(line) else { return nil }
+    return num
+}
+
+func readPosAndAlpha() -> (Int, String)? {
+    guard let line = readLine() else { return nil }
+    let temp = line.split(separator: " ").map(String.init)
+    if temp.count != 2 { return nil }
+    if let index = Int(temp[0]) {
+        return (index, temp[1])
+    }
+    return nil
+}
+
+func getLimit(_ arr: [(Int, String)]) -> Int {
+    var limit = -1
+    for (pos, alpha) in arr {
+        if pos > limit {
+            limit = pos
+        }
+    }
+    return limit
+}
+
+func solution(_ arr: [(Int, String)]) -> Int {
+    var answer = 0
+
+    let limit = getLimit(arr)
+
+    var row = Array(repeating: "", count: limit + 1)
+
+    for (pos, alpha) in arr {
+        row[pos] = alpha
+    }
+
+    for start in 0..<(limit - 1) {
+        if row[start] == "" {
+            continue
+        }
+        for end in (start + 1)..<limit {
+            if row[end] == "" {
+                continue
+            }
+
+            let value = end - start
+            if value < answer {
+                continue
+            }
+
+            var stack = 0
+            for index in start...end {
+                if row[index] == "G" {
+                    stack += 1
+                }
+                if row[index] == "H" {
+                    stack -= 1
+                }
+            }
+
+            if stack == 0 {
+                answer = value
+            }
+        }        
+    }
+
+    return answer
+}
+
+func main() {
+    guard let n = readNum() else { return }
+    let arr = (1...n).compactMap{_ in readPosAndAlpha()}
+    print(solution(arr))
+}
+
+main()
