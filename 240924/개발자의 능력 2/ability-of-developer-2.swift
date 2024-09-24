5608332b19fb45f88a906fc148f2588595454768
geppetto <saejinpark951210@gmail.com>
2024-09-24 22:20:44 +0900

    Title: 개발자의 능력 2, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240924/개발자의 능력 2/ability-of-developer-2.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+precedencegroup ForwardPipe {
+    associativity: left
+}
+
+infix operator |> : ForwardPipe
+
+func |> <T, U> (value: T, function: (T) -> U) -> U {
+    return function(value)
+}
+
+func readNums() -> [Int]? {
+    guard let line = readLine() else { return nil }
+    let temp = line.split(separator: " ").map(String.init)
+    let nums = temp.compactMap{Int($0)}
+    return nums
+}
+
+func solution(_ nums: [Int]) -> Int {
+    let sortedNums = nums.sorted()
+    var sums = [Int]()
+
+    for i in 1...nums.count / 2 {
+        let sum = sortedNums[i - 1] + sortedNums[sortedNums.count - i - 1]
+        sums.append(sum)
+    }
+
+    return abs(sums.min()! - sums.max()!)
+}
+
+func main() {
+    guard let nums = readNums() else { return }
+    print(solution(nums))
+}
+
+main()
