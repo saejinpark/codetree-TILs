0b6d2a6ad3213109cfc279a18976eacce441d292
geppetto <saejinpark951210@gmail.com>
2025-01-04 22:37:30 +0900

    Title: 상해버린 치즈, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250104/상해버린 치즈/rotten-cheese.py
@@ -0,0 +1,40 @@
+answer = 0
+
+n, m, d, s = map(int, input().split())
+
+eat_log = []
+pain_log = []
+
+for _ in range(d):
+    p, m, t = map(int, input().split())
+    eat_log.append((p, m, t))
+
+for _ in range(s):
+    p, t = map(int, input().split())
+    pain_log.append((p, t))
+
+for i in range(1, m + 1):
+    test_case = True
+
+    for (p, t) in pain_log:
+
+        if not test_case:
+            break
+        
+        for (_p, m, _t) in eat_log:
+
+            if p == _p and m == i and not _t < t:
+                test_case = False
+            
+            if not test_case:
+                break
+
+    if test_case:
+        filtered_eat_log = list(filter(lambda x : x[1] == i, eat_log))
+    
+        answer = max(answer, len(filtered_eat_log))
+
+print(answer)
+
+
+
