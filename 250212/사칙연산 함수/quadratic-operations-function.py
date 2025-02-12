16044f9ca817e76644b2aff640ee6b1598f13f5c
geppetto <saejinpark951210@gmail.com>
2025-02-12 17:00:26 +0900

    Title: 사칙연산 함수, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250212/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,18 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+# Write your code here!
+
+if o not in ["+", "-", "/", "*"]:
+    print("False")
+
+else:
+    if o == "+":
+        print(a, o, c, a + c)
+    elif o == "-":
+        print(a, o, c, a - c)
+    elif o == "/":
+        print(a, o, c, a // c)
+    else:
+        print(a, o, c, "=", a * c)
