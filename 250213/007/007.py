347e1e845f9b59a18f359df91771febd871ea3fb
geppetto <saejinpark951210@gmail.com>
2025-02-13 22:47:51 +0900

    Title: 007, Time: 38ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250213/007/007.py
@@ -0,0 +1,7 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+# Write your code here!
+print(f"secret code : {secret_code}")
+print(f"meeting point : {meeting_point}")
+print(f"time : {time}")
