0757685cb58ac1d457e313cad14d1cff09da4db3
geppetto <saejinpark951210@gmail.com>
2025-02-14 10:28:32 +0900

    Title: 폭탄 해제, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250214/폭탄 해제/bomb-removal.py
@@ -0,0 +1,8 @@
+unlock_code, wire_color, seconds = input().split()
+seconds = int(seconds)
+
+# Write your code here!
+
+print(f"code : {unlock_code}")
+print(f"color : {wire_color}")
+print(f"second : {seconds}")
